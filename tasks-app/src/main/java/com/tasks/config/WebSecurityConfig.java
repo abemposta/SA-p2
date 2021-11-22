d0ecddd191660fe03214401d823f874c371be6da
javids15 <javier.diazs@udc.es>
2021-11-22 16:53:38 +0100

    SAPP-03 - Role based access control

--- tasks-app/src/main/java/com/tasks/config/WebSecurityConfig.java
+++ tasks-app/src/main/java/com/tasks/config/WebSecurityConfig.java
@@ -41,9 +41,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.GET,  "/swagger-ui.html").permitAll()
             .antMatchers(HttpMethod.GET,  "/swagger-resources/**").permitAll()
             .antMatchers(HttpMethod.GET,  "/v2/api-docs").permitAll()
-            .anyRequest().permitAll();
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            // ENDPOINT /projects
+                .antMatchers(HttpMethod.POST,  "/api/projects").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,  "/api/projects/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,  "/api/projects/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,  "/api/projects/{id}/tasks").permitAll()
+
+
+            // ENDPOINT /tasks
+                .antMatchers(HttpMethod.POST,  "/api/tasks").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,  "/api/tasks/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,  "/api/tasks/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,  "/tasks/{id}/changeState").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,  "/tasks/{id}/changeResolution").hasRole("USER")
+                .antMatchers(HttpMethod.POST,  "/tasks/{id}/changeProgress").hasAnyRole("USER","ADMIN")
+                .antMatchers(HttpMethod.POST,  "/comments").hasAnyRole("USER","ADMIN")
+                .antMatchers(HttpMethod.GET,  "/comments/{id}").hasAnyRole("USER","ADMIN")
+
+
+            // ENDPOINT /users
+                .antMatchers(HttpMethod.GET,  "/api/users").hasRole("ADMIN")
+
+                .anyRequest().permitAll();
+
+        //b2 ?¿?
+        //b3 permitAll a los 3 endpoints que dice enunciado
+        //b4 resto de enpoints check si tienen rol, si no anyRequest().denyAll
+
+        //3 endpoint.hasRole("role") o en el endpoint anotacion @PreAuthorize("hasRole('ROLE_ADMIN')") tb vale @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER')")
+        //3 check en entidades p.e @PreAuthorize o @PostAuthorize( obj.user == authentication.user ) //check admin borre sus vainas
+
+        //b1,b5
+        http.addFilter( new JwtAuthorizationFilter(this.tokenProvider, customAuthenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
